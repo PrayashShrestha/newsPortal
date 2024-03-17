db286ad5d64ebbe3e8bde699d289a04a56195bd5
arahman6 <arahman.isrt@gmail.com>
2024-03-16 23:48:13 -0500

    added AppBar component and intrgate in homepage

--- frontend/src/app/homepage/page.js
+++ frontend/src/app/homepage/page.js
@@ -1,17 +1,22 @@
 import * as React from 'react';
 import data from "../data.json"
 import CardComp from '../_components/CardComp';
+import ResponsiveAppBar from '../_components/AppBar';
 import { Container, Grid, Paper } from '@mui/material';
 
- export default function Homepage(){
+export default function Homepage() {
   return (
+    <div>
+    <ResponsiveAppBar/>
     <Container >
- <Grid container spacing={3}>
+      <Grid container spacing={3}>
         {data.map((article) => (
           <Grid item xs={12} sm={6} md={4} lg={3} key={article.id}>
             <CardComp title={article.title} content={article.content} imageUrl={data.imageUrl} />
           </Grid>
         ))}
-      </Grid></Container>
-    )
- }
+      </Grid>
+    </Container>
+    </div>
+  )
+}
