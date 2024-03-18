d2fecce89d1ce304bb955b46906dd6e8cded20b8
sereneblack <sandhya@fusemachines.com>
2024-03-18 15:44:51 -0500

    feat: adds responsivebar

--- frontend/src/pages/editorPage.js
+++ frontend/src/pages/editorPage.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import CreatePostPage from "../components/TextEditor";
 import TextEditor from "../components/TextEditor";
+import { Container } from "@mui/material";
+import ResponsiveAppBar from "../components/AppBar";
 
 export default function EditorPage() {
   return (
-    <div>
-      <h1>Write your article here</h1>
-      <TextEditor />
-    </div>
+    <>
+      <ResponsiveAppBar />{" "}
+      <Container maxWidth="md" sx={{ marginY: 2, marginX: "auto" }}>
+        <h1>Write your article here</h1>
+        <TextEditor />
+      </Container>
+    </>
   );
 }
