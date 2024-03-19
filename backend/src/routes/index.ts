fb43a541f7f9448c7294c49f4fa53ea670798d64
prayash27 <prayash.shrestha@wiseyak.com>
2024-03-19 15:46:32 -0500

    added apis for news

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import userRouter from "./User";
 import authRouter from "./Auth";
+import newsRouter from "./News";
 import editorsRouter from "../controllers/editorsRouter";
 
 export const routes = express.Router();
 
 routes.use("/user", userRouter);
 routes.use("/auth", authRouter);
+routes.use("/news", newsRouter);
 routes.use("/editor", editorsRouter);
