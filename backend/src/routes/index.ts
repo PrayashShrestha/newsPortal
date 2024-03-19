3546188da3ea96c680b838eaaeb0ca1308ce6473
sereneblack <sandhya@fusemachines.com>
2024-03-19 14:34:59 -0500

    feat: add editor router

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import userRouter from "./User";
 import authRouter from "./Auth";
+import editorsRouter from "../controllers/editorsRouter";
 
 export const routes = express.Router();
 
 routes.use("/user", userRouter);
 routes.use("/auth", authRouter);
+routes.use("/editor", editorsRouter);
