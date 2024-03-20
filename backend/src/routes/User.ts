b8ad27e707ad0d8b1ff531c1537f1b074ea73a81
prayash27 <prayash.shrestha@wiseyak.com>
2024-03-20 11:23:19 -0500

    added api for getUserNews

--- backend/src/routes/User.ts
+++ backend/src/routes/User.ts
@@ -1,3 +1,4 @@
+import { getNewsByUser } from "./../controllers/User";
 import {
   getUsers,
   getUser,
@@ -19,8 +20,9 @@ userRouter.post("/role", authenticate, getUsersByRole);
 userRouter.get("/:id", authenticate, getUser);
 userRouter.post("/", authenticate, createUser);
 userRouter.put("/:id", authenticate, updateUser);
-userRouter.put("update-password/:id", updateUserPassword);
+userRouter.put("/update-password/:id", updateUserPassword);
 userRouter.delete("/:id", authenticate, deleteUser);
-userRouter.post("forgot-password/:id", forgotUserPassword);
+userRouter.post("/forgot-password/:id", forgotUserPassword);
+userRouter.get("/get-user-news/:id", getNewsByUser);
 
 export default userRouter;
